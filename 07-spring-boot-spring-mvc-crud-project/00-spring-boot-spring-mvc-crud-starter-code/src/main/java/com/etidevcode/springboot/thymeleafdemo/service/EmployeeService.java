47f55b5ac0a7a91b6753788ab185ba16bc8cef58
Etidevcode <etixicode@gmail.com>
2024-05-31 13:15:59 +0200

    SPRING BOOT 3

---
+++ 07-spring-boot-spring-mvc-crud-project/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/etidevcode/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.etidevcode.springboot.thymeleafdemo.service;
+
+
+import com.etidevcode.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Spring Boot 3
 
 ![Spring Boot 3](images/spring-boot-3-spring-6-and-hibernate-thumbnail-small.png)
 
+
+<hr>
+
+## MVC CRUD PROJET
+
+![mvc-crud-projet](images/projet-mvc-crud1.jpeg)
+![mvc-crud-projet](images/projet-mvc-crud2.jpeg)
